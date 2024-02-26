b73e1e5e7d73683d50edc5304b02fb66d1ccf8c2
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-02-26 09:26:36 -0300

    Update CSS and add new SVG icons

--- data/cumpri.js
+++ data/cumpri.js
@@ -3,13 +3,12 @@ const rafaelData = {
     name: "Rafael Custodio Cumpri",
     description: "Um desenvolvedor web que estuda no SENAI, gosta de programar por amor e está sempre em busca de novos desafios. Apaixonado por tecnologia e inovação, sempre empenhado em aprender e aplicar novos conhecimentos em seus projetos.",
     tecnologias: [
-      { name: "Java", img: "/url_da_imagem_java" },
-      { name: "JavaScript", img: "/url_da_imagem_js" },
-      { name: "Next.js", img: "/url_da_imagem_next" },
-      { name: "EXPO", img: "/url_da_imagem_expo" },
-      { name: "CSS", img: "/url_da_imagem_css" },
-      { name: "Python", img: "/url_da_imagem_python" },
-      { name: "React", img: "/url_da_imagem_react" }
+      { name: "Java", img: "/../../java.svg" },
+      { name: "JavaScript", img: "/../../js.svg" },
+      { name: "Next.js", img: "/../../next-js.svg" },
+      { name: "CSS", img: "/../../css3-alt.svg" },
+      { name: "Python", img: "/../../python.svg" },
+      { name: "React", img: "/../../react.svg" }
     ],
     projetos: [
       { name: "Projeto 1", img: "/url_da_imagem_projeto1", link: "url_do_projeto1" },
